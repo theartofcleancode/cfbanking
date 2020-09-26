a6742531274b08895f87d505577738611ddeb134
moctarjallo <moctarjallo@gmail.com>
2020-09-26 14:00:42 +0000

    implemented create account and refactored main to bank

--- cfbanking/bank.py
+++ cfbanking/bank.py
@@ -1,22 +1,28 @@
 import banking
 import pythonapi as api
-from consolefile.domain import Console, File
-
+import consolefile as cf
 class Bank:
     def __init__(self):
-        self.input = Console()
-        self.output = Console()
-        self.client_output = File('./clients.txt')
+        self.reader = cf.Reader('console')
+        self.writer = cf.Writer('./accounts.txt')
+        self.printer = cf.Writer('console')
 
     def create_account(self):
-        client =  self.input.read('First Name'), \
-                  self.input.read('Last Name'), \
-                  self.input.read('Address'), \
-                  self.input.read('Balance')
+        client =  self.reader.read('First Name'), \
+                self.reader.read('Last Name'), \
+                self.reader.read('Address'), \
+                self.reader.read('Balance')
         request = api.TupleRequest(client)
 
         create_account = banking.CreateAccount()
         account = create_account.execute(request)
 
-        response = api.Response(str(account))
-        self.client_output.write(response.data)
+        response = api.TextResponse([account])
+        self.writer.write(response.data)
+        self.printer.write(response.data)
+
+    def run(self):
+        self.create_account()
+
+if __name__ == '__main__':
+    Bank().run()
