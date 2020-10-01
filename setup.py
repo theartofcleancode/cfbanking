4205afc570f3a418deabfc16371eb04c8d4d632d
moctarjallo <moctarjallo@gmail.com>
2020-10-01 05:26:43 +0000

    packaging to github: ssh instead of https

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     dependency_links = [
-        'banking @ git+https://github.com/theartofcleancode/banking.git@python#egg=banking-1.0.1',
-        'pythonapi @ git+https://github.com/theartofcleancode/pythonapi.git#egg=pythonapi-1.0.0',
-        'consolefile @ git+https://github.com/theartofcleancode/consolefile.git#egg=consolefile-1.0.0'
+        'banking @ git+ssh://github.com/theartofcleancode/banking.git@python#egg=banking-1.0.1',
+        'pythonapi @ git+ssh://github.com/theartofcleancode/pythonapi.git#egg=pythonapi-1.0.0',
+        'consolefile @ git+ssh://github.com/theartofcleancode/consolefile.git#egg=consolefile-1.0.0'
     ],
     entry_points={
         'console_scripts': [
